1ba40d9a57d5c24ebd3af46876846688ca5feaf9
deadlockinside <mytempmail228@internet.ru>
2025-09-21 22:09:23 +0300

    Add StockItem entity

---
+++ WMS.Domain/Entities/StockItem.cs
@@ -0,0 +1,8 @@
+﻿namespace WMS.Domain.Entities;
+
+public class StockItem
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Quantity { get; set; }
+}
